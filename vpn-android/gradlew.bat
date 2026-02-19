cba8fc17bcec0f8db7a0b3f168eae0bbdd58d4ae
konstantinPovalaev <118921284+konstantinPovalaev@users.noreply.github.com>
2026-02-19 22:34:58 +0300

    Add files via upload

---
+++ vpn-android/gradlew.bat
@@ -0,0 +1,9 @@
+﻿@if "%DEBUG%" == "" @echo off
+@rem
+@rem  Gradle start script for Windows
+@rem
+@setlocal
+set DIR=%~dp0
+if "%DIR%" == "" set DIR=.
+set JAVA_EXE=java.exe
+"%JAVA_EXE%" -classpath "%DIR%\gradle\wrapper\gradle-wrapper.jar" org.gradle.wrapper.GradleWrapperMain %*
